4c9c453e411c78adbf836c0590770685c8100e46
AndresArturo <griyftan@gmail.com>
2016-08-30 20:17:09 -0500

    HttpReader first implementation

--- src/input/HttpReader.java
+++ src/input/HttpReader.java
@@ -1,5 +1,40 @@
 package input;
 
-public class HttpReader {
+import java.io.IOException;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HttpReader {
+	
+	
+	private OkHttpClient client;
+	private Request request;
+	
+	
+	public HttpReader(String httpUrl) {
+		client = new OkHttpClient();
+		request = new Request.Builder()
+				.url(httpUrl)
+				.build();
+	}
+
+
+	public String read() throws IOException {
+		Response response;
+		String reponseBody;
+		
+		response = client.newCall(request).execute();
+		reponseBody = response.body().string();
+		response.close();
+		
+		if(!response.isSuccessful())
+			throw new IOException("HTTP server responded with an error status code");
+
+		return reponseBody;
+	}
+	
+	
+	
 }
--- test/input/HttpReaderTest.java
+++ test/input/HttpReaderTest.java
@@ -3,19 +3,18 @@
  */
 package input;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import okhttp3.internal.http.HttpHeaders;
-import okhttp3.mockwebserver.*;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 
 
@@ -34,12 +33,14 @@ import okhttp3.mockwebserver.*;
 public class HttpReaderTest {
 	
 	private final MockWebServer server = new MockWebServer();
+	private final String serviceURL = "/api/v2/position/suggest/en/leipzig";
 	private HttpReader reader;
 
 
 	@Before
 	public void setUp() throws Exception {
 	   server.start();
+	   reader = new HttpReader(server.url(serviceURL).toString());
 	}
 	
 
@@ -51,53 +52,55 @@ public class HttpReaderTest {
 
 	@Test
 	public void testSuccessfulResponse() throws IOException {
-		final String body;
+		final String body = "[{\"_id\":376809,\"key\":null,\"name\":\"Leipzig\",\"fullName\":\"Leipzig, Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":51.33962,\"longitude\":12.37129},\"locationId\":8982,\"inEurope\":true,\"countryId\":56,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null,\"names\":{\"pt\":\"Lípsia\",\"ru\":\"Лейпциг\",\"it\":\"Lipsia\",\"zh\":\"莱比锡\",\"cs\":\"Lipsko\",\"pl\":\"Lipsk\"},\"alternativeNames\":{\"it\":[\"Sassonia - Lipsia\"],\"es\":[\"Sajonia - Leipzig\"],\"pl\":[\"Saksonia - Lipsk\"],\"pt\":[\"Saxónia - Lípsia\"],\"fr\":[\"Saxe - Leipzig\"],\"ru\":[\"Саксония - Лейпциг\"],\"de\":[\"Sachsen - Leipzig\"],\"zh\":[\"萨克森 - 莱比锡\"],\"en\":[\"Saxony - Leipzig\"],\"cs\":[\"Sasko - Lipsko\"],\"sv\":[\"Sachsen - Leipzig\"],\"ca\":[\"Saxònia - Leipzig\"],\"nl\":[\"Saksen - Leipzig\"]}}]";
 		MockResponse serverResponse;
-		HttpResponse response;
 		
-		body = "[{\"_id\":376809,\"key\":null,\"name\":\"Leipzig\",\"fullName\":\"Leipzig, Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":51.33962,\"longitude\":12.37129},\"locationId\":8982,\"inEurope\":true,\"countryId\":56,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null,\"names\":{\"pt\":\"Lípsia\",\"ru\":\"Лейпциг\",\"it\":\"Lipsia\",\"zh\":\"莱比锡\",\"cs\":\"Lipsko\",\"pl\":\"Lipsk\"},\"alternativeNames\":{\"it\":[\"Sassonia - Lipsia\"],\"es\":[\"Sajonia - Leipzig\"],\"pl\":[\"Saksonia - Lipsk\"],\"pt\":[\"Saxónia - Lípsia\"],\"fr\":[\"Saxe - Leipzig\"],\"ru\":[\"Саксония - Лейпциг\"],\"de\":[\"Sachsen - Leipzig\"],\"zh\":[\"萨克森 - 莱比锡\"],\"en\":[\"Saxony - Leipzig\"],\"cs\":[\"Sasko - Lipsko\"],\"sv\":[\"Sachsen - Leipzig\"],\"ca\":[\"Saxònia - Leipzig\"],\"nl\":[\"Saksen - Leipzig\"]}}]";
 		serverResponse = new MockResponse()
 				.setResponseCode(HttpURLConnection.HTTP_OK)
 				.addHeader("Content-Type", "application/json;charset=utf-8")
 				.setBody(body);
 		
-	    server.enqueue(serverResponse);	    
-	    reader = new HttpReader(server.url("/api/v2/position/suggest/en/leipzig"));
-	    response = reader.read();
+	    server.enqueue(serverResponse);
 	    
-	    assertTrue(reponse.isValid());
-	    assertEquals(body, reponse.getBody());
+	    assertEquals(body, reader.read());
 	}
 	
-	@Test
+	
+	@Test(expected=IOException.class)
 	public void testConnectionError() throws IOException {
-		final String body;
-		HttpResponse response;
-		
-		reader = new HttpReader(server.url("/api/v2/position/suggest/en/leipzig"));
 		server.shutdown();
-	    response = reader.read();
-	    
-	    assertFalse(reponse.isValid());
+	    reader.read();
 	}
+	
 	
 	@Test
 	public void testBadResponse() throws IOException {
-		final String body;
 		MockResponse serverResponse;
-		HttpResponse response;
 		
-		body = "[{\"_id\":376809,\"key\":null,\"name\":\"Leipzig\",\"fullName\":\"Leipzig, Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":51.33962,\"longitude\":12.37129},\"locationId\":8982,\"inEurope\":true,\"countryId\":56,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null,\"names\":{\"pt\":\"Lípsia\",\"ru\":\"Лейпциг\",\"it\":\"Lipsia\",\"zh\":\"莱比锡\",\"cs\":\"Lipsko\",\"pl\":\"Lipsk\"},\"alternativeNames\":{\"it\":[\"Sassonia - Lipsia\"],\"es\":[\"Sajonia - Leipzig\"],\"pl\":[\"Saksonia - Lipsk\"],\"pt\":[\"Saxónia - Lípsia\"],\"fr\":[\"Saxe - Leipzig\"],\"ru\":[\"Саксония - Лейпциг\"],\"de\":[\"Sachsen - Leipzig\"],\"zh\":[\"萨克森 - 莱比锡\"],\"en\":[\"Saxony - Leipzig\"],\"cs\":[\"Sasko - Lipsko\"],\"sv\":[\"Sachsen - Leipzig\"],\"ca\":[\"Saxònia - Leipzig\"],\"nl\":[\"Saksen - Leipzig\"]}}]";
-		serverResponse = new MockResponse()
-				.setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
-				.addHeader("Content-Type", "application/json;charset=utf-8")
-				.setBody(body);
-		
-	    server.enqueue(serverResponse);	    
-	    reader = new HttpReader(server.url("/api/v2/position/suggest/en/leipzig"));
-	    response = reader.read();
+		serverResponse = new MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND);
+	    server.enqueue(serverResponse);
 	    
-	    assertFalse(reponse.isValid());
-	    assertEquals(body, reponse.getBody());
+	    try {
+	    	reader.read();
+	    	assertTrue(false);
+	    } catch (IOException e) {
+			assertEquals("HTTP server responded with an error status code", e.getMessage());
+		}
+	}
+	
+	
+	/**
+	 * Tests the query of a real HTTP service.
+	 * Appends a real server's hostname to the simulated service URL to make a real query.
+	 * Uncomment to execute test only when the mock server is not enough.
+	 * @throws IOException 
+	 */
+	@Test
+	public void testRealRequest() throws IOException {
+		final String body = "[{\"_id\":376809,\"key\":null,\"name\":\"Leipzig\",\"fullName\":\"Leipzig, Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":51.33962,\"longitude\":12.37129},\"locationId\":8982,\"inEurope\":true,\"countryId\":56,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null,\"names\":{\"pt\":\"Lípsia\",\"ru\":\"Лейпциг\",\"it\":\"Lipsia\",\"zh\":\"莱比锡\",\"cs\":\"Lipsko\",\"pl\":\"Lipsk\"},\"alternativeNames\":{\"it\":[\"Sassonia - Lipsia\"],\"es\":[\"Sajonia - Leipzig\"],\"pl\":[\"Saksonia - Lipsk\"],\"pt\":[\"Saxónia - Lípsia\"],\"fr\":[\"Saxe - Leipzig\"],\"ru\":[\"Саксония - Лейпциг\"],\"de\":[\"Sachsen - Leipzig\"],\"zh\":[\"萨克森 - 莱比锡\"],\"en\":[\"Saxony - Leipzig\"],\"cs\":[\"Sasko - Lipsko\"],\"sv\":[\"Sachsen - Leipzig\"],\"ca\":[\"Saxònia - Leipzig\"],\"nl\":[\"Saksen - Leipzig\"]}},{\"_id\":425121,\"key\":null,\"name\":\"Böhlen (Leipzig)\",\"fullName\":\"Böhlen (Leipzig), Germany\",\"iata_airport_code\":null,\"type\":\"location\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":51.20061,\"longitude\":12.38622},\"locationId\":124459,\"inEurope\":true,\"countryId\":56,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null,\"names\":{},\"alternativeNames\":{}},{\"_id\":314829,\"key\":null,\"name\":\"Leipzig\",\"fullName\":\"Leipzig (LEJ), Germany\",\"iata_airport_code\":\"LEJ\",\"type\":\"airport\",\"country\":\"Germany\",\"geo_position\":{\"latitude\":51.41974,\"longitude\":12.22014},\"locationId\":null,\"inEurope\":true,\"countryId\":56,\"countryCode\":\"DE\",\"coreCountry\":true,\"distance\":null,\"names\":{\"it\":\"Lipsia\"},\"alternativeNames\":{}}]";
+		   
+	    reader = new HttpReader("http://api.goeuro.com" + serviceURL);
+	    
+	    assertEquals(body, reader.read());
 	}
 }
