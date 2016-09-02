bf537e3acb155b209ce51e3d143093c61c934f5a
AndresArturo <griyftan@gmail.com>
2016-09-02 16:39:28 -0500

    First implementation of SimpleFilter through TDD

---
+++ src/processor/SimpleFilter.java
@@ -0,0 +1,55 @@
+package processor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SimpleFilter is the class responsible for cleaning unwanted 
+ * attributes off of a Map.
+ * A SimpleFilter object provides the algorithm to generate new
+ * Maps containing only desired attributes.
+ * @author Andres Arturo Sanchez Dorantes
+ *
+ */
+public class SimpleFilter {
+	
+	/**
+	 * The list of attributes to preserve.
+	 */
+	private ArrayList<String> wantedAttr;
+	
+	
+	public SimpleFilter(List<String> attributes) {
+		this.wantedAttr = new ArrayList<>(attributes);
+	}
+	
+
+	public SimpleFilter(String... attributes) {
+		this(Arrays.asList(attributes));
+	}
+	
+
+	/**
+	 * Creates a new Map containing only the desired attributes.
+	 * The new Map contains the attributes resulting from the 
+	 * intersection of the sets of the original Map's keys and the 
+	 * desired keys.
+	 * @param originalObj The original Map containing all the attributes.
+	 * @return The filtered Map.
+	 */
+	public Map<String, Object> process(Map<String, Object> originalObj) {
+		Map<String, Object> newObj = new HashMap<>();
+		
+		for(String attr : wantedAttr)
+			if(originalObj.containsKey(attr))
+				newObj.put(attr, originalObj.get(attr));
+		
+		return newObj;
+	}
+
+	
+
+}
---
+++ test/processor/SimpleFilterTest.java
@@ -0,0 +1,72 @@
+/**
+ * Test class created following the TDD process.
+ */
+package processor;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the SimpleFilter class.
+ * The class responsibility is to filter unwanted attributes off of a Map.
+ * <p>
+ * Example: If there is a Map ["attr1":"val1","attr2.attr1":val2,"attr2.attr2":"val3"]
+ * and only "attr1" and "arrt2.attr2" are wanted then the resulting map should be
+ * ["attr1":"val1","attr2.attr2":"val3"]
+ * <p>
+ * The functionality of SimpleFilter should be:
+ * <ul>
+ * <li>Create a new Map containing only the desired attributes.
+ * </ul>
+ * @author Andres Arturo Sanchez Dorantes
+ *
+ */
+public class SimpleFilterTest {
+
+	private Map<String, Object> originalObj;
+	private SimpleFilter filter;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		originalObj = new HashMap<String, Object>();
+		originalObj.put("attr1", "val1");
+		originalObj.put("attr2.attr2_1", "val2");
+		originalObj.put("attr2.attr2_2", "val3");
+		originalObj.put("attr2.attr2_3", "val4");
+		originalObj.put("attr3.attr3_1.0", "val5");
+		originalObj.put("attr3.attr3_1.1", "val6");
+		
+		filter = new SimpleFilter("attr1", "attr2.attr2_2", "attr3.attr3_1.1");
+	}
+
+	@Test
+	public void testFilterAttributes() {
+		Map<String, Object> result = filter.process(originalObj);
+		
+		assertEquals(3, result.values().size());
+		assertEquals("val1", result.get("attr1"));
+		assertEquals("val3", result.get("attr2.attr2_2"));
+		assertEquals("val6", result.get("attr3.attr3_1.1"));
+		assertFalse(result.containsKey("attr2.attr2_1"));
+	}
+	
+	
+	@Test
+	public void testMissingKey() {
+		originalObj.remove("attr2.attr2_2");
+		Map<String, Object> result = filter.process(originalObj);
+		
+		assertEquals(2, result.values().size());
+		assertEquals("val1", result.get("attr1"));
+		assertEquals("val6", result.get("attr3.attr3_1.1"));
+	}
+
+}
